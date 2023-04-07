6c2a589a03e9213782c4d519646aeec97d3146d5
egoist138 <rasulbagirov7777@gmail.com>
2023-04-07 14:07:18 +0400

    'connect2'

--- connect.go
+++ connect.go
@@ -16,7 +16,7 @@ func main() {
 	defer db.Close()
 
 	// ?????????? ??????? ? ????????? ???????????
-	rows, err := db.Query("SELECT id, name FROM users")
+	rows, err := db.Query("SELECT * FROM pg_database")
 	if err != nil {
 		panic(err)
 	}
@@ -24,13 +24,17 @@ func main() {
 
 	for rows.Next() {
 		var id int
-		var name string
-		err = rows.Scan(&id, &name)
+		var datname, datlocprovider, datistemplate, datallowconn, datcollate, datctype string
+		var datdba, encoding, datconnlimit, datfrozenxid, datminmxid, dattablespace int
+		var daticulocale, atcollversion, datacl *string
+
+		err = rows.Scan(&id, &datname, &datdba, &encoding, &datlocprovider, &datistemplate, &datallowconn, &datconnlimit, &datfrozenxid, &datminmxid, &dattablespace, &datcollate, &datctype, &daticulocale, &atcollversion, &datacl)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(id, name)
+		fmt.Printf("id=%d, datname=%s, datdba=%d, encoding=%d, datlocprovider=%s, datistemplate=%s, datallowconn=%s, datconnlimit=%d, datfrozenxid=%d, datminmxid=%d, dattablespace=%d, datcollate=%s, datctype=%s, daticulocale=%v, datcollversion=%v, atcollversion=%s, datacl=%v\n", id, datname, datdba, encoding, datlocprovider, datistemplate, datallowconn, datconnlimit, datfrozenxid, datminmxid, dattablespace, datcollate, datctype, daticulocale, atcollversion, datacl)
 	}
+
 	if err = rows.Err(); err != nil {
 		panic(err)
 	}
